fbca5c31f6cf4e4e60a2a79d760489ebd213d379
Anthony Brown <bruinbrown@gmail.com>
2015-07-28 15:26:25 +0100

    Started work on tests for the counters

---
+++ Eventual.Tests/CounterTests.fs
@@ -0,0 +1,23 @@
+﻿namespace Eventual.Tests
+
+open Eventual.Counters
+open System.Collections.Immutable
+open System.Collections.Generic
+open NUnit.Framework
+
+[<TestFixture>]
+module GCounterTests =
+    
+    let kvp key value =
+        KeyValuePair(key, value)
+    
+    [<Test>]
+    let ``Adding 2 GCounters should give the correct total``() =
+        let state1 = [kvp "node1" 546L; kvp "node2" 547L].ToImmutableDictionary()
+        let state2 = [kvp "node1" 547L; kvp "node2" 548L].ToImmutableDictionary()
+        let expectedValue = 1095L //This is max node1 + max node2
+        let gcounter1 = GCounter(state1)
+        let gcounter2 = GCounter(state2)
+        let gcounterMerge = gcounter1.Merge(gcounter2)
+        Assert.AreEqual(expectedValue, gcounterMerge.Value)
+        ()
