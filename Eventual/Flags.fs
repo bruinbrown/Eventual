103ac679f35bc4f84add202b1c6f9328f0991197
Anthony Brown <bruinbrown@gmail.com>
2015-07-28 15:04:39 +0100

    Added simple CvRDT flag

---
+++ Eventual/Flags.fs
@@ -0,0 +1,15 @@
+﻿namespace Eventual.Flags
+
+open Eventual
+
+[<Sealed>]
+type Flag(state:bool) =
+    inherit AbstractCvRDT<Flag, bool>()
+
+    static member Unset = Flag(false)
+
+    override this.State = state
+
+    override this.Merge(that:Flag) =
+        if that.State then that
+        else this
