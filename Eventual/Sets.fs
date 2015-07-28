698be7e51393ba4be79792215b06a5894c3ae9bc
Anthony Brown <bruinbrown@gmail.com>
2015-07-28 15:14:02 +0100

    Added grow only set

---
+++ Eventual/Sets.fs
@@ -0,0 +1,21 @@
+﻿namespace Eventual.Sets
+
+open System.Collections.Immutable
+open Eventual
+
+[<Sealed>]
+type GSet<'a>(elements:IImmutableSet<'a>) =
+    inherit AbstractCvRDT<GSet<'a>, IImmutableSet<'a>>()
+
+    static member Empty = GSet(ImmutableHashSet<_>.Empty)
+
+    override x.Merge(that) =
+        let elements = x.State.Union(that.State)
+        GSet(elements)
+
+    override x.State = elements
+
+    member x.Add(value:'a) =
+        GSet(elements.Add(value))
+
+
